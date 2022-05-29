a5a6ddde4eb533caad17657920ce464e693fe5dd
Joris van Riel <j.van.riel@hotmail.com>
2022-05-29 14:37:54 +0200

    GameManager can handle start game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,39 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
-    private CharacterSheet characterSheet;
+    [SerializeField] CharacterSheet characterSheet;
+    [SerializeField] GameObject titleScreenPrefab;
+    [SerializeField] GameObject dungeonPrefab;
+    [SerializeField] GameObject characterPrefab;
+    [SerializeField] GameObject skillScreenPrefab;
+
+    private GameObject titleScreen;
+    private GameObject skillScreen;
+
+    public GameObject SkillScreen { get => skillScreen; }
+
+    private void Awake()
+    {
+        titleScreen = Instantiate(titleScreenPrefab);
+        skillScreen = Instantiate(skillScreenPrefab);
+        Instantiate(dungeonPrefab);
+        skillScreen.SetActive(false);
+    }
 
     public void StartGame()
     {
         characterSheet.Reset();
+        titleScreen.SetActive(false);
+        Instantiate(characterPrefab);
+    }
+
+    public void HandleCharacterDeath()
+    {
+        skillScreen.SetActive(true);
+    }
+
+    public void RespawnCharacter()
+    {
+        skillScreen.SetActive(false);
     }
 }
--- Assets/Tests/PlayMode/GameManagerTest.cs
+++ Assets/Tests/PlayMode/GameManagerTest.cs
@@ -10,6 +10,35 @@ public class GameManagerTests : TestsBase
     private const int gameManagerTestScene = 8;
 
     [UnityTest]
+    public IEnumerator OnAwakeGameManagerShouldInstantiateDungeon()
+    {
+        yield return AssertObjectWithTagIsInstantiatedAtAwake("Dungeon");
+    }
+
+    [UnityTest]
+    public IEnumerator OnAwakeGameManagerShouldInstantiateTitleScreen()
+    {
+        yield return AssertObjectWithTagIsInstantiatedAtAwake("Dungeon");
+    }
+
+    [UnityTest]
+    public IEnumerator GameManagerShouldInstantiateSkillScreenOnAwake()
+    {
+        yield return LoadScene(gameManagerTestScene);
+        GameManager gameManager = GetSutComponent<GameManager>();
+        Assert.IsTrue(gameManager.SkillScreen != null, "Skill Screen is not instantiated.");
+    }
+
+    [UnityTest]
+    public IEnumerator GameManagerShouldDisableSkillScreenOnAwak()
+    {
+        yield return LoadScene(gameManagerTestScene);
+        GameManager gameManager = GetSutComponent<GameManager>();
+        Assert.IsFalse(gameManager.SkillScreen.activeInHierarchy, "Skill screen should be disabled on awake");
+
+    }
+
+    [UnityTest]
     public IEnumerator GameManagerShouldShouldResetTheCharacterSheetOnStartGame()
     {
         yield return LoadScene(gameManagerTestScene);
@@ -21,10 +50,68 @@ public class GameManagerTests : TestsBase
 
         GameManager gameManager = GetSutComponent<GameManager>();
         gameManager.StartGame();
-        
+
         Assert.AreEqual(0, characterSheet.ExperiencePoints);
         Assert.AreEqual(characterSheet.StartingAttack, characterSheet.Attack);
         Assert.AreEqual(characterSheet.StartingDefence, characterSheet.Defence);
 
     }
+
+    [UnityTest]
+    public IEnumerator GamemanagerShouldDisableTitleScreenOnStartGame()
+    {
+        yield return LoadScene(gameManagerTestScene);
+        GameManager gameManager = GetSutComponent<GameManager>();
+        GameObject titleScreen = GetObjectWithTag("TitleScreen");
+        gameManager.StartGame();
+        yield return new WaitForSeconds(1);
+        Assert.IsFalse(titleScreen.activeInHierarchy, "Titlescreen is not disabled.");
+    }
+
+    [UnityTest]
+    public IEnumerator GameManagerShouldInstantiatCharacterOnStartGame()
+    {
+        yield return LoadScene(gameManagerTestScene);
+        GameManager gameManager = GetSutComponent<GameManager>();
+        gameManager.StartGame();
+        GameObject character = GameObject.FindGameObjectWithTag("Character");
+
+        Assert.IsTrue(character != null, "Character should be instantiated on start game.");
+    }
+
+
+
+    [UnityTest]
+    public IEnumerator GameManagerShouldShowSkillScreenWhenCharacterDies()
+    {
+        yield return LoadScene(gameManagerTestScene);
+        GameManager gameManager = GetSutComponent<GameManager>();
+
+        gameManager.HandleCharacterDeath();
+
+        yield return Wait();
+
+        Assert.IsTrue(gameManager.SkillScreen.activeInHierarchy, "SkillScreen should be shown after character dies");
+    }
+
+    [UnityTest]
+    public IEnumerator GameManagerShouldDisableSkillScreenOnRespawn()
+    {
+        yield return LoadScene(gameManagerTestScene);
+        GameManager gameManager = GetSutComponent<GameManager>();
+        gameManager.HandleCharacterDeath(); 
+        yield return Wait();
+
+        gameManager.RespawnCharacter();
+        yield return Wait();
+
+        Assert.IsFalse(gameManager.SkillScreen.activeInHierarchy, "SkillScreen Should be disabled after character respawn");
+    }
+
+    private IEnumerator AssertObjectWithTagIsInstantiatedAtAwake(string tag)
+    {
+        yield return LoadScene(gameManagerTestScene);
+        Assert.IsTrue(GameObject.FindGameObjectWithTag(tag) != null, $"{tag} not instatiated");
+    }
+   
 }
